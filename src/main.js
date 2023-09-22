de2424eda4237559d3b03b6b969acf7dab771ff9
nathan <side_nathan@hotmail.com>
2023-09-22 19:35:56 -0400

    Add fade in animation

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-particlesJS.load("particles-container", "../assets/particles.json");
+document.addEventListener("DOMContentLoaded", () => {
+  particlesJS.load("particles-container", "../assets/particles.json");
+});
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show-fade");
+    } else {
+      entry.target.classList.remove("show-fade");
+    }
+  });
+})
+
+const hiddenElements = document.querySelectorAll(".hidden-fade");
+hiddenElements.forEach((element) => observer.observe(element));
