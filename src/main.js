61658b301d061a5a218a493b8f158d4edae8b8f3
nathan <side_nathan@hotmail.com>
2023-09-24 01:59:39 -0400

    Finish nav bar responsive behavior

--- src/main.js
+++ src/main.js
@@ -11,3 +11,46 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll(".hidden-fade");
 hiddenElements.forEach((element) => observer.observe(element));
+
+const navLinks = document.querySelectorAll(".nav a");
+
+navLinks.forEach(link => {
+  link.addEventListener("click", (e) => {
+    // Remove 'active' class from all links
+    navLinks.forEach(link => {
+      link.classList.remove('active');
+    });
+
+    // Add 'active' class to the clicked link
+    link.classList.add('active');
+  });
+});
+
+
+const sections = document.querySelectorAll('section');
+function isInView(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+  );
+}
+
+// Function to update active navigation item
+function updateActiveNavItem() {
+  if (window.scrollY === 0) {
+    navLinks.forEach(link => link.classList.remove('active'));
+    navLinks[0].classList.add('active'); // Assuming the first link is "About"
+    return;
+  }
+
+  sections.forEach((section, index) => {
+    if (isInView(section)) {
+      navLinks.forEach(link => link.classList.remove('active'));
+      navLinks[index].classList.add('active');
+    }
+  });
+}
+
+// Event listener for scroll
+window.addEventListener('scroll', updateActiveNavItem);
--- src/project-viewer.js
+++ src/project-viewer.js
@@ -1,10 +1,13 @@
 import TypeWriter from "typewriter-effect/dist/core";
 
 let currentProject = null;
-let numCommandsRun = 0;
 
 const terminalCommands = {
-  hw1: "gcc hw1.c"
+  hw1: "gcc hw1.c",
+  hw3: "make",
+  midterm: "make",
+  hw5: "make",
+  hw7: "make all",
 }
 
 const terminalTypeWriter = new TypeWriter("#terminal-emulator", {
@@ -17,6 +20,7 @@ terminalTypeWriter.start();
 const projectForm = document.getElementById("project-form");
 projectForm.addEventListener("submit", (e) => {
   e.preventDefault();
+  projectForm.querySelector('button[type="submit"]').blur();
 
   const selectedProject = document.getElementById("project-dropdown").value;
   handleProjectSubmit(selectedProject);
@@ -26,12 +30,23 @@ function handleProjectSubmit(selectedProject) {
   currentProject = selectedProject;
   switch (selectedProject) {
     case "ip-hw1":
-      numCommandsRun = 0;
       terminalTypeWriter.deleteAll();
-      terminalTypeWriter.changeDelay(50)
+      terminalTypeWriter.changeDelay(50);
       terminalTypeWriter.typeString(terminalCommands.hw1);
       terminalTypeWriter.start();
       break;
+    case "ip-hw3":
+      terminalTypeWriter.deleteAll();
+      terminalTypeWriter.changeDelay(25);
+      terminalTypeWriter.typeString(terminalCommands.hw3);
+      terminalTypeWriter.start();
+      break;
+    case "ip-midterm":
+      terminalTypeWriter.deleteAll();
+      terminalTypeWriter.changeDelay(25);
+      terminalTypeWriter.typeString(terminalCommands.hw3);
+      terminalTypeWriter.start();
+      break;  
   }
 }
 
@@ -39,14 +54,30 @@ document.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     switch (currentProject) {
       case "ip-hw1":
-        if (numCommandsRun === 0) {
-          terminalTypeWriter.deleteAll();
-          terminalTypeWriter.typeString("./a.out")
-          terminalTypeWriter.start();
-          numCommandsRun++;
-        } else if (numCommandsRun === 1) {
-          
-        }
+        terminalTypeWriter.deleteAll();
+        terminalTypeWriter.typeString("./a.out")
+        terminalTypeWriter.start();
+        break;
+      case "ip-hw3":
+        terminalTypeWriter.deleteAll();
+        terminalTypeWriter.typeString("./hw3");
+        terminalTypeWriter.start();
+        break;
+      case "ip-midterm":
+        terminalTypeWriter.deleteAll();
+        terminalTypeWriter.typeString("./project");
+        terminalTypeWriter.start();
+        break;
+      case "ip-hw5":
+        terminalTypeWriter.deleteAll();
+        terminalTypeWriter.typeString("./phone_db");
+        terminalTypeWriter.start();
+        break;
+      case "ip-hw7":
+        terminalTypeWriter.deleteAll();
+        terminalTypeWriter.typeString("./project");
+        terminalTypeWriter.start();
+        break;
     }
   }
 });
