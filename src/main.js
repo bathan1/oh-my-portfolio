142c75edc48d7f75afacad111d0f4a4ea3c6f9ad
nathan <side_nathan@hotmail.com>
2023-09-25 21:45:58 -0400

    Finish nav bar of new-build site

--- src/main.js
+++ src/main.js
@@ -1,55 +1,22 @@
-// Get the fade in animation (from FireShip)
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show-fade");
-    } else {
-      entry.target.classList.remove("show-fade");
-    }
-  });
-});
+// make clicking hamburger icon slide logo out
+const hamburgerSvg= document.getElementById("hamburger-svg");
+hamburgerSvg.addEventListener("click", (e) => {
+  const logoDiv = document.getElementById("logo-div");
+  if (!logoDiv.classList.contains("logo-slideout")) {
+    logoDiv.classList.remove("logo-slidein");
+    logoDiv.classList.add("logo-slideout");
 
-const hiddenElements = document.querySelectorAll(".hidden-fade");
-hiddenElements.forEach((element) => observer.observe(element));
+    const navListContainer = document.getElementById("nav-list-container");
+    navListContainer.classList.remove("nav-container-fadeout");
+    navListContainer.classList.add("nav-container-fadein");
+  } else {
+    const navListContainer = document.getElementById("nav-list-container");
+    navListContainer.classList.remove("nav-container-fadein");
+    navListContainer.classList.add("nav-container-fadeout");
 
-const navLinks = document.querySelectorAll(".nav a");
-
-navLinks.forEach((link) => {
-  link.addEventListener("click", (e) => {
-    // Remove 'active' class from all links
-    navLinks.forEach((link) => {
-      link.classList.remove("active");
-    });
-
-    // Add 'active' class to the clicked link
-    link.classList.add("active");
-  });
-});
-
-const sections = document.querySelectorAll("section");
-function isInView(element) {
-  const rect = element.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
-  );
-}
-
-// Function to update active navigation item
-function updateActiveNavItem() {
-  if (window.scrollY === 0) {
-    navLinks.forEach((link) => link.classList.remove("active"));
-    navLinks[0].classList.add("active"); // Assuming the first link is "About"
-    return;
+    logoDiv.classList.remove("logo-slideout");
+    logoDiv.classList.add("logo-slidein");
   }
-
-  sections.forEach((section, index) => {
-    if (isInView(section)) {
-      navLinks.forEach((link) => link.classList.remove("active"));
-      navLinks[index].classList.add("active");
-    }
-  });
-}
-
-// Event listener for scroll
-window.addEventListener("scroll", updateActiveNavItem);
+  
+  hamburgerSvg.classList.toggle("opened");
+});
